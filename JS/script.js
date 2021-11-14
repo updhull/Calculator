d97df90524cad4105bae875d750bc386390bffb3
updhull <abdullahi.ismail@student.moringaschool.com>
2021-11-14 09:38:14 +0300

    Commit2

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = add(number1, number2);
+  alert(result);
+
+var multiply =function(number1, number2) {
+    return number1 * number2;
+};
+
+var number1 =parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter another number"));
+
+var result = multiply(number1, number2);
+alert(result);
+
+
+ 
